8218f1c9645534166ebffb0fc1b721981f588206
sylar7 <andy34021@126.com>
2014-03-12 14:24:49 +0800

    Point Class and input method

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -6,11 +6,33 @@ class Dijkstra{
 	private static final int EDGESIZE = 7035;
 //	define the spatial network
 	private static Edge[] edges = new Edge[EDGESIZE];
+//	define point Departure Point
+	private static Point departure;
+//	define point Destination Point
+	private static Point destination;
 	
 	public static void main(String args[]){
 		readEdges();
 		writeEdges();
+		getPoints();	
+		readPoints();
 	}
+	
+//	get Point from Input
+	public static void getPoints(){
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please input Departure and Destination Points(separated by space):");
+		departure = new Point(in.nextInt(), in.nextInt(), in.nextDouble());
+		destination = new Point(in.nextInt(), in.nextInt(), in.nextDouble());
+		in.close();
+	}
+	
+//	read points
+	public static void readPoints(){
+		System.out.println(departure.getFstNode() + "\t" + departure.getSndNode() + "\t" +departure.getPosition());
+		System.out.println(destination.getFstNode() + "\t" + destination.getSndNode() + "\t" +destination.getPosition());
+	}
+	
 //	read all the edges
 	public static void readEdges(){
 		String src = "src/spatial_network.txt";
@@ -34,22 +56,26 @@ class Dijkstra{
 				edges[n] = edge;
 				n++;
 			}
+			br.close();
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 	}
+	
 //	write all the edges
 	public static void writeEdges(){
 		for(int n = 0; n < EDGESIZE; n++){
 			writeEdge(n);
 		}
 	}
+	
 //	write one specific edge
 	public static void writeEdge(int n){
 		Edge wedge = edges[n];
-		System.out.println(wedge.edgeId + "\t" + wedge.fstNode + "\t" + wedge.sndNode + "\t" + wedge.distance);
+		System.out.println(wedge.getEdgeId() + "\t" + wedge.getFstNode() + "\t" + wedge.getSndNode() + "\t" + wedge.getDistance());
 		
 	}
+	
 //	define class Edge
 	public static class Edge{
 		int edgeId;
@@ -62,6 +88,47 @@ class Dijkstra{
 			this.fstNode = fstNode;
 			this.sndNode = sndNode;
 			this.distance = distance;
+		}
+		
+		public int getEdgeId(){
+			return edgeId;
+		}
+		
+		public int getFstNode(){
+			return fstNode;
+		}
+		
+		public int getSndNode(){
+			return sndNode;
+		}
+		
+		public double getDistance(){
+			return distance;
+		}
+		
+	}
+	
+//	define class Point
+	public static class Point{
+		int fstNode;
+		int sndNode;
+		double position;
+		
+		public Point(int fstNode, int sndNode, double position){
+			this.fstNode = fstNode; 
+			this.sndNode = sndNode;
+			this.position = position;
+		}
+		
+		public int getFstNode(){
+			return fstNode;
+		}
+		
+		public int getSndNode(){
+			return sndNode;
+		}
+		public double getPosition(){
+			return position;
 		}
 	}
 }
